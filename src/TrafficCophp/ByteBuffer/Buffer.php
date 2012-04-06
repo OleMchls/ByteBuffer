d31b9563ffaaebb32bf27a6e346f90537d624022
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-04-06 17:24:14 +0200

    debugging on travis also sucks

--- src/TrafficCophp/ByteBuffer/Buffer.php
+++ src/TrafficCophp/ByteBuffer/Buffer.php
@@ -51,7 +51,7 @@ class Buffer extends AbstractBuffer {
 		}
 		if (($format == 'N' || $format == 'V') && PHP_INT_SIZE <= 4) {
 			list(, $h, $l) = unpack(strtolower($format).'*', $encoded);
-			$correctingValue = ($format == 'N') ? 0x010000 : 0x000100;
+			$correctingValue = ($format == 'N') ? 0x010000 : 0x100000;
 			$result = ($l + ($h * $correctingValue));
 		} else {
 			list(, $result) = unpack($format, $encoded);
